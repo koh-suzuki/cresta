c51fcbd1254b74b53367d76fa2fd97e64d79ce23
aoshiro <koh.sss0629@gmail.com>
2021-03-14 02:19:23 +0900

    ハンバーガーメニュー フェードインアウト　追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-$('.burger-btn').on('click',function(){
-  $('.burger-btn').toggleClass('close');
-  $('.header__nav').fadeToggle(500);
-  $('body').toggleClass('noscroll');
+$(function(){
+  $('.burger-btn').on('click',function(){
+    $('.burger-btn').toggleClass('close');
+    $('.header__nav').fadeToggle(700);
+    $('body').toggleClass('noscroll');
+  });
 });
