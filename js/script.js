63ddb9e5ff8497ab996ad2020ba3ba69cfca0a0e
aoshiro <koh.sss0629@gmail.com>
2021-03-14 18:23:46 +0900

    topへ戻るボタン  formテキストエリアのフォントサイズ変更　追加

--- js/script.js
+++ js/script.js
@@ -4,4 +4,18 @@ $(function(){
     $('.header__nav').fadeToggle(700);
     $('body').toggleClass('noscroll');
   });
+
+  // 100px スクロールしたらボタン表示
+  var pagetop = $('#page__top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 800) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
 });
